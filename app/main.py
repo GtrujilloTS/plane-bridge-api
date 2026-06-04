158fdcfe31c850569f5be3e4360caa1e6039ac16
Giovanni Trujillo Silvas <gtrujill0@outlook.com>
2026-06-04 17:34:02 -0600

    FEAT: update CORS middleware settings to allow OPTIONS method and disable credentials

--- app/main.py
+++ app/main.py
@@ -24,9 +24,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["GET"],
+    allow_credentials=False,
+    allow_methods=["GET", "OPTIONS"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 
